61ac451db45a5b13b83cb981962021aa782f5189
Pranshu Dixit <dixitpranshu01@gmail.com>
2023-08-19 17:31:57 +0530

    Added Function

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse, RedirectResponse
 from pydantic import BaseModel
 from starlette.staticfiles import StaticFiles
-import random
-import string
+import hashlib
 import os
-
 
 app = FastAPI()
 
-# In-memory storage for short-to-long URL mapping
+# In-memory storage for long-to-short URL mapping
 url_mapping = {}
 
 class URLItem(BaseModel):
@@ -24,22 +22,20 @@ def home():
 
 @app.post("/shorten")
 def shorten_url(url_item: URLItem):  # Use URLItem model for request body
-    short_url = generate_short_url()
-    url_mapping[short_url] = url_item.long_url
+    short_url = generate_short_url(url_item.long_url)
+    url_mapping[url_item.long_url] = short_url
     return {"short_url": f"/{short_url}"}
 
 @app.get("/{short_url}")
 def redirect_to_original(short_url: str):
-    if short_url in url_mapping:
-        long_url = url_mapping[short_url]
-        return RedirectResponse(url=long_url)  # Redirect to the original long URL
-    else:
-        raise HTTPException(status_code=404, detail="Short URL not found")
+    for long_url, generated_short_url in url_mapping.items():
+        if generated_short_url == short_url:
+            return RedirectResponse(url=long_url)  # Redirect to the original long URL
+    raise HTTPException(status_code=404, detail="Short URL not found")
 
-def generate_short_url():
-    characters = string.ascii_letters + string.digits
-    short_url = ''.join(random.choice(characters) for _ in range(6))
-    return short_url
+def generate_short_url(long_url: str):
+    hash_object = hashlib.md5(long_url.encode())  # Use MD5 hash
+    return hash_object.hexdigest()[:6]  # Use the first 6 characters of the hash
 
 if __name__ == "__main__":
     import uvicorn
